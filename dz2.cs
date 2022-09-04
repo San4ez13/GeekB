d59604a08f9e8f26df66209c85216372ebe02c39
Bolwoi <Duplovich13@yandex.ru>
2022-09-04 20:06:49 +0300

    dz2

---
+++ dz2.cs
@@ -0,0 +1,55 @@
+﻿// Задача 25
+/*
+Console.WriteLine();
+
+int Exponentiation(int numberA, int numberB)
+
+{
+    int result = 1;
+    for(int i=1; i <= numberB; i++)
+
+    {
+        result = result * numberA;
+    }
+
+        return result;
+
+}
+
+  Console.Write("Введите число A: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exponentiation(numberA, numberB);
+  Console.WriteLine("Ответ: " + exponentiation);
+*/
+
+//Задача 27
+/*
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN)
+  {
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+
+    {
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Общая сумма в числе: " + sumNumber);
+*/
+
+
+
